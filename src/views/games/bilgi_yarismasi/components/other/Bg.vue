d821ef5dd74f1faf30a599d667c3f8ff5620f050
Ahmet ŞIK <koneviahmet@gmail.com>
2024-06-07 16:50:53 +0300

    dvm

--- src/views/games/bilgi_yarismasi/components/other/Bg.vue
+++ src/views/games/bilgi_yarismasi/components/other/Bg.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="relative">
-        <img class="absolute top-0 w-full h-full left-0" src="/img/games/yazim_yanlisi/icon-02.svg" alt="">
+        <img class="absolute top-0 w-full h-full left-0" src="" alt="">
         <div class="absolute w-full h-full flex items-center justify-center">
             <slot></slot>
         </div>
